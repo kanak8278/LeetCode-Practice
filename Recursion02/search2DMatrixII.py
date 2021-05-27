634a8f3bda0619200b03cbc30af286146103eed4
Kanak Raj <kanak8278@gmail.com>
2021-05-27 14:08:43 +0530

    search2DMatrixII.py AC

--- Recursion02/search2DMatrixII.py
+++ Recursion02/search2DMatrixII.py
@@ -3,26 +3,16 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
 
-        def fn(low, high):
-            h_i, h_j = high
-            l_i, l_j = low
-            piv_i = int((h_i + l_i) / 2)
-            piv_j = int((h_j + l_j) / 2)
-
-            if matrix[piv_i][piv_j] == target:
-                print("True", matrix[piv_i][piv_j])
-                return True
-            if matrix[piv_i][piv_j] > target:
-                print("greater", matrix[piv_i][piv_j])
-                fn((l_i, l_j), (piv_i, piv_j))
-                fn((l_i, piv_j), (piv_i, h_j))
-                fn((piv_i, l_i), (h_i, piv_j))
-            if matrix[piv_i][piv_j] < target:
-                print("smaller", matrix[piv_i][piv_j])
-                fn((l_i, piv_j), (piv_i, h_j))
-                fn((piv_i, l_i), (h_i, piv_j))
-                fn((piv_i, piv_j), (h_i, h_j))
-            else:
+        def fn(row, col):
+            if row == m or col == -1:
                 return False
+            if matrix[row][col] == target:
+                return True
+            if matrix[row][col] > target:
+                col -= 1
+                return fn(row, col)
+            if matrix[row][col] < target:
+                row += 1
+                return fn(row, col)
 
-        return fn((0, 0), (m, n))
+        return fn(0, n - 1)
