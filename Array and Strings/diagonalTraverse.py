a939643e9ffcce32421a876cec2f4eba4cc37b8c
Kanak Raj <kanak8278@gmail.com>
2021-05-31 14:08:52 +0530

    diagonalTraverse.py Space Complexity O(1) AC

--- Array and Strings/diagonalTraverse.py
+++ Array and Strings/diagonalTraverse.py
@@ -1,5 +1,3 @@
-# Space: O(n)
-# Time: O(n)
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         l = []
@@ -7,24 +5,35 @@ class Solution:
         n = len(mat[0])
 
         def fn(i, j):
+            # print(i, j)
             if 0 <= i < m and 0 <= j < n:
-                mtrx = []
-                # mtrx.append(mat[i][j])
-                x = i
-                y = j
-                while x < m and y >= 0:
-                    mtrx.append(mat[x][y])
-                    x = x + 1
-                    y = y - 1
-                # print(mtrx)
-                if (i + j) % 2 == 1:
-                    l.extend(mtrx)
+                # print("Inside the First Case")
                 if (i + j) % 2 == 0:
-                    l.extend(mtrx[::-1])
-                if j < n - 1:
-                    return fn(i, j + 1)
-                else:
-                    return fn(i + 1, j)
+                    while True:
+                        l.append(mat[i][j])
+                        if i - 1 >= 0 and j + 1 <= n - 1:
+                            i -= 1
+                            j += 1
+                        else:
+                            break
+                    if j < n - 1:
+                        return fn(i, j + 1)
+                    else:
+                        return fn(i + 1, j)
+
+                elif (i + j) % 2 == 1:
+                    # print("Run Case 2")
+                    while True:
+                        l.append(mat[i][j])
+                        if i + 1 < m and j - 1 >= 0:
+                            i += 1
+                            j -= 1
+                        else:
+                            break
+                    if i < m - 1:
+                        return fn(i + 1, j)
+                    else:
+                        return fn(i, j + 1)
 
         fn(0, 0)
         return l
